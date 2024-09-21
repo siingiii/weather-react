f5fb1eabeea1ffea71c8ec8d07df7a82f839b5c0
Singita Innocentia B <singitabaloyi@yahoo.com>
2024-09-21 23:26:38 +0200

    Made footer visible

--- src/App.js
+++ src/App.js
@@ -54,19 +54,21 @@ function App() {
               />
             </li>
           </ul>
-          <footer>
-            Open
+          
+        </>
+      )}
+      <footer>
+            This project was coded by Singita Innocentia and is{" "}
             <a
               href="https://github.com/siingiii/weather-react"
               target="_blank"
               rel="noopener noreferrer"
             >
-              {" "}
-              source code
-            </a>
+              
+              open-source code on Github</a>{" "}and hosted on{" "}
+            <a href="https://sprinkle-weather-react.netlify.app" target="_blank" rel="noopener noreferrer">Netlify
+              </a>.
           </footer>
-        </>
-      )}
     </div>
   );
 }

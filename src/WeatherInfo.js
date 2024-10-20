97fe202fa8b573f1fab9303b6f2d27101b2fb45e
Singita Innocentia B <singitabaloyi@yahoo.com>
2024-10-20 15:51:42 +0200

    Remove precipitation display from weather forecast due to API limitations

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -35,7 +35,6 @@ export default function WeatherInfo(props) {
         </div>
         <div className="col-6">
           <ul>
-            <li>Precipitation: 0%</li>
             <li>Humidity: {props.data.humidity}%</li>
             <li>Wind: {props.data.wind}km/h</li>
           </ul>
